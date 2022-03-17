e9dfb3277aa41b4244f40524439305d9d852f171
Michal Trnka <michal.trnka@outlook.com>
2022-03-18 00:44:08 +0100

    new geojsons

--- public/conqueror/conqueror.js
+++ public/conqueror/conqueror.js
@@ -1,7 +1,7 @@
 var geojson;
 var okresy = [{"name":"Benešov","cnt":95459},{"name":"Beroun","cnt":86160},{"name":"Blansko","cnt":105708},{"name":"Brno-město","cnt":385913},{"name":"Brno-venkov","cnt":206300},{"name":"Bruntál","cnt":92693},{"name":"Břeclav","cnt":112828},{"name":"Česká Lípa","cnt":100756},{"name":"České Budějovice","cnt":186462},{"name":"Český Krumlov","cnt":60516},{"name":"Děčín","cnt":128834},{"name":"Domažlice","cnt":59926},{"name":"Frýdek-Místek","cnt":207756},{"name":"Havlíčkův Brod","cnt":94217},{"name":"Hodonín","cnt":153225},{"name":"Hradec Králové","cnt":162661},{"name":"Cheb","cnt":90188},{"name":"Chomutov","cnt":122157},{"name":"Chrudim","cnt":103199},{"name":"Jablonec n.N.","cnt":88200},{"name":"Jeseník","cnt":38779},{"name":"Jičín","cnt":79702},{"name":"Jihlava","cnt":110522},{"name":"Jindřichův Hradec","cnt":90604},{"name":"Karlovy Vary","cnt":115446},{"name":"Karviná","cnt":256394},{"name":"Kladno","cnt":158799},{"name":"Klatovy","cnt":85726},{"name":"Kolín","cnt":96001},{"name":"Kroměříž","cnt":105569},{"name":"Kutná Hora","cnt":73404},{"name":"Liberec","cnt":169878},{"name":"Litoměřice","cnt":117278},{"name":"Louny","cnt":85191},{"name":"Mělník","cnt":104659},{"name":"Mladá Boleslav","cnt":123659},{"name":"Most","cnt":111775},{"name":"Náchod","cnt":109550},{"name":"Nový Jičín","cnt":148074},{"name":"Nymburk","cnt":94884},{"name":"Olomouc","cnt":230408},{"name":"Opava","cnt":174899},{"name":"Ostrava-město","cnt":326018},{"name":"Pardubice","cnt":168423},{"name":"Pelhřimov","cnt":71914},{"name":"Písek","cnt":69843},{"name":"Plzeň-jih","cnt":62389},{"name":"Plzeň-město","cnt":188045},{"name":"Plzeň-sever","cnt":74940},{"name":"Praha","cnt":1268796},{"name":"Praha-východ","cnt":157146},{"name":"Praha-západ","cnt":131231},{"name":"Prachatice","cnt":50010},{"name":"Prostějov","cnt":107859},{"name":"Přerov","cnt":130082},{"name":"Příbram","cnt":112816},{"name":"Rakovník","cnt":54993},{"name":"Rokycany","cnt":47458},{"name":"Rychnov n.K.","cnt":77829},{"name":"Semily","cnt":73605},{"name":"Sokolov","cnt":89961},{"name":"Strakonice","cnt":69786},{"name":"Svitavy","cnt":103245},{"name":"Šumperk","cnt":121299},{"name":"Tábor","cnt":101115},{"name":"Tachov","cnt":51917},{"name":"Teplice","cnt":125498},{"name":"Trutnov","cnt":118174},{"name":"Třebíč","cnt":111693},{"name":"Uherské Hradiště","cnt":141467},{"name":"Ústí n.L.","cnt":118228},{"name":"Ústí n.O.","cnt":136760},{"name":"Vsetín","cnt":142420},{"name":"Vyškov","cnt":88154},{"name":"Zlín","cnt":190488},{"name":"Znojmo","cnt":111380},{"name":"Žďár n.S.","cnt":117219}]
-var southWest = L.latLng(53, 8),
-    northEast = L.latLng(45, 23),
+var southWest = L.latLng(35, -20),
+    northEast = L.latLng(70, 50),
     bounds = L.latLngBounds(southWest, northEast);
 var map;
 
@@ -258,17 +258,31 @@ function showLevel(targetLevel){
     //     maxZoom: 10,
     //     zoomControl: false
     // }).setView([45, 15], targetZoom);
-    geojsonLayer = readTextFile("./distictsCzechiaLow.json", function(text){
+    // geojsonLayer = readTextFile("./distictsCzechiaLow.json", function(text){
+    //     var data = JSON.parse(text);
+    //     data.features.forEach(function(region) {
+    //             region.reset = true;
+    //             region.level = true;
+    //             var okres = okresy.find(item =>  {
+    //                 return item.name == region.properties.name;
+    //             })
+    //             if (typeof okres !== "undefined"){
+    //                 region.properties.id = okres.cnt
+    //             } else region.properties.id = 0
+    //             geojson = L.geoJson(region, {style: style, onEachFeature: onEachFeature}).addTo(map);
+    //         });
+    //     });
+    geojsonLayer = readTextFile("./kraje.json", function(text){
         var data = JSON.parse(text);
         data.features.forEach(function(region) {
                 region.reset = true;
                 region.level = true;
-                var okres = okresy.find(item =>  {
-                    return item.name == region.properties.name;
-                })
-                if (typeof okres !== "undefined"){
-                    region.properties.id = okres.cnt
-                } else region.properties.id = 0
+                // var okres = okresy.find(item =>  {
+                //     return item.name == region.properties.name;
+                // })
+                // if (typeof okres !== "undefined"){
+                //     region.properties.id = okres.cnt
+                // } else region.properties.id = 0
                 geojson = L.geoJson(region, {style: style, onEachFeature: onEachFeature}).addTo(map);
             });
         });
@@ -313,7 +327,7 @@ function showLevelOut(trgt){
 
 function init(){
     map = L.map('map', {
-        //maxBounds: bounds,
+        maxBounds: bounds,
         minZoom: 5,
         maxZoom: 10,
         zoomControl: false
@@ -321,11 +335,11 @@ function init(){
     var src = 0;
     var trgt = 0;
     map.on("zoomstart", function (e) { 
-        console.log("ZOOMSTART source: " + e.sourceTarget._zoom + " target: " + e.target._zoom, e); 
+        //console.log("ZOOMSTART source: " + e.sourceTarget._zoom + " target: " + e.target._zoom, e); 
         src = e.sourceTarget._zoom;
     });
     map.on("zoomend", function (e) {
-        console.log("ZOOMEND " + e.sourceTarget._zoom + " target: " + e.target._zoom, e); 
+        //console.log("ZOOMEND " + e.sourceTarget._zoom + " target: " + e.target._zoom, e); 
         var trgt = e.target._zoom;
         if (zoomIn(src, trgt)){
             if (showLevelIn(trgt)){
@@ -343,9 +357,9 @@ function init(){
             }
         }
     });
-    map.on('moveend', function() { 
-        console.log(map.getBounds());
-    });
+    // map.on('moveend', function() { 
+    //     console.log(map.getBounds());
+    // });
     // var myMovingMarker = L.Marker.movingMarker([[48.8567, 2.3508],[50.45, 30.523333]],
     //     [20000]).addTo(map);
         //myMovingMarker.start();
@@ -403,21 +417,24 @@ function init(){
         //     marker2.bindPopup('<b>Welcome to Bucarest !</b><br>GitHub Page: <a target="\\blanck" href="https://github.com/ewoken/Leaflet.MovingMarker"><img src="github.png"></a>', {closeOnClick: false})
         //     .openPopup();
         // });
-
+    // original
     //var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFsdHJua2EiLCJhIjoiY2wwdXNqaDB0MHdsZDNsbjVjMDB6ZDR5ZiJ9.mONacYmtCytV8Y96HlN8xg', {
+    // old geo
     //var tiles = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//         var tiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {    
-//             //var tiles = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {    
-// 	maxZoom: 18,
-// 	attribution: 'Conqueror &copy; <a href="https://www.datagin.cz">Datagin.cz</a>',
-// 	id: 'mapbox/streets-v11',
-// 	tileSize: 512,
-// 	zoomOffset: -1,
-//     ext: 'jpg'
-// }).addTo(map);
-
-var layer = new L.StamenTileLayer('terrain');
-map.addLayer(layer);
+    // like painting
+    //var tiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {    
+    // satelite
+    // var tiles = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {    
+    //     maxZoom: 18,
+    //     attribution: 'Conqueror &copy; <a href="https://www.datagin.cz">Datagin.cz</a>',
+    //     id: 'mapbox/streets-v11',
+    //     tileSize: 512,
+    //     zoomOffset: -1,
+    //     ext: 'jpg'
+    // }).addTo(map);
+        
+    var layer = new L.StamenTileLayer('terrain');
+    map.addLayer(layer);
 
     readTextFile("./czechia.json", function(text){
         var countryGeoJson = JSON.parse(text);
